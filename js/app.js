e1fcc5adce778d6ed3c12800f4a56a78d92e88ed
sho327 <49551360+sho327@users.noreply.github.com>
2025-02-20 02:55:40 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// 外部コンポーネント呼び出し
+const Card = ReactBootstrap.Card;
+const Badge = ReactBootstrap.Badge;
+const Button = ReactBootstrap.Button;
+const { useState } = React;
+
+const App = () => {
+    return (
+        <div>
+            aaaa
+        </div>
+    )
+}
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container)
+root.render(<App/>)
